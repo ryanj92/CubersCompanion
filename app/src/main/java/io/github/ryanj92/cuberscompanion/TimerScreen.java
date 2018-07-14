c985975da52a2354ede6bfb7f4e20d1440b8b11a
RJ <ryanj92@live.co.uk>
2018-07-14 15:37:29 +0100

    Initial commit

---
+++ app/src/main/java/io/github/ryanj92/cuberscompanion/TimerScreen.java
@@ -0,0 +1,13 @@
+package io.github.ryanj92.cuberscompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_screen);
+    }
+}
